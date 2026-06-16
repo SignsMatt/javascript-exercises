7d7839c41ff59a9e2eadc0fe692f65eb760d144a
Matt Alexander <85204672+SignsMatt@users.noreply.github.com>
2026-06-15 21:51:05 -0600

    Completed removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+
+// First Try:
+
+// const removeFromArray = function (arr, ...args) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (args.includes(arr[i])) {
+//       arr.splice(i, 1);
+//       i--;
+//     }
+//   }
+//   return arr;
+// };
+
+const removeFromArray = function (arr, ...args) {
+  let newArray = [];
+  arr.forEach((element) => {
+    if (!args.includes(element)) {
+      newArray.push(element);
+    }
+  });
+  return newArray;
 };
 
 // Do not edit below this line
