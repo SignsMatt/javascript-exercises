31b53c817931bb73c296e854dd65d6dc7d4fc667
Matt Alexander <85204672+SignsMatt@users.noreply.github.com>
2026-06-15 22:18:42 -0600

    Completed sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if (a < 0 || b < 0) return "ERROR";
 
+  let result = 0;
+  let min = a < b ? a : b;
+  let max = a < b ? b : a;
+
+  for (let i = min; i <= max; i++) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
